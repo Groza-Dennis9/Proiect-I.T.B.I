7fe77bc23ec76c0f8b4ae2db8bc857a4a55c371c
Groza-Dennis9 <denygroza4@gmail.com>
2026-01-01 18:01:33 +0000

    Crearea cazului in care un utilizator este activ Corectarea erorii de redactare

--- UserFS_GrozaDenisa-Georgiana.sh
+++ UserFS_GrozaDenisa-Georgiana.sh
@@ -1,9 +1,19 @@
-#!/bin/bash/
+#!/bin/bash
 
 ROOT_DIR="users_logged"
+cd
 mkdir -p "$ROOT_DIR"
 
 while true;
 do
+	current_users=$(who | cut -d' ' -f1)
+	for user in $current_users;
+	do
+		user_dir="$ROOT_DIR/$user"
+		mkdir -p "$user_dir"
+		ps -u "$user" -o pid,command --no-headers > "$user_dir/procs"
+		rm -f "$user_dir/lastlogin"	
+	done
+
 	sleep 30
 done
